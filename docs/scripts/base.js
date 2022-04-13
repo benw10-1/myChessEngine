bdb0e847fb750936cd7246414a4a4afec191bf2d
Ben Wirth <benjaminwirth10@gmail.com>
2022-04-13 16:35:31 -0700

    whoops

--- docs/scripts/base.js
+++ docs/scripts/base.js
@@ -2,6 +2,6 @@ var boardObj
 
 function loadEls() {
     boardObj = new Chessboard(document.getElementById("board"), {
-        fen: "5K2/3P4/8/8/4p3/8/8/5k2 w - - 0 1"
+        // fen: "5K2/3P4/8/8/4p3/8/8/5k2 w - - 0 1"
     })
 }
