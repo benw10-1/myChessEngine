6bccf75d4b215f6461954997f15ad39524735fad
Ben Wirth <benjaminwirth10@gmail.com>
2022-04-16 21:08:41 -0700

    some improvments kind of

--- docs/scripts/base.js
+++ docs/scripts/base.js
@@ -2,6 +2,6 @@ var boardObj
 
 function loadEls() {
     boardObj = new CB(document.getElementById("board"), {
-        fen: "1k1r1bnr/ppp2ppp/2n5/3pp1q1/1P1P2b1/2PBP1P1/P3NP1P/RNBQK2R w KQ - 0 1"
+        // fen: "1k1r1bnr/ppp2ppp/2n5/3pp1q1/1P1P2b1/2PBP1P1/P3NP1P/RNBQK2R w KQ - 0 1"
     })
 }
