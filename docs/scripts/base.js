aafda8cb86a40d9069d96965638fabcefba45fe6
Ben Wirth <benjaminwirth10@gmail.com>
2022-04-18 23:29:31 -0700

    oops again

--- docs/scripts/base.js
+++ docs/scripts/base.js
@@ -2,6 +2,6 @@ var boardObj
 
 function loadEls() {
     boardObj = new CB(document.getElementById("board"), {
-        fen: "2Rk3r/3nbpp1/p2p3p/8/8/2Q1P3/1P3PPP/1NB1KBNR b K - 0 1"
+        // fen: "2Rk3r/3nbpp1/p2p3p/8/8/2Q1P3/1P3PPP/1NB1KBNR b K - 0 1"
     })
 }
